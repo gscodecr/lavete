5e22bf65ba56c55aeb253b2dd324921c0dc4a96a
Gerardo <gerardo@gscodecr.com>
2026-02-18 13:27:15 -0600

    Add changes for domain

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    root_path="/lavete" # Deployed under /lavete
 )
 
 # Set all CORS enabled origins
